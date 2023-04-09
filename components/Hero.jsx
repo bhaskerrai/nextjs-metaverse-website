82dec9bad687ed06a3966a4d10a5859affe1496e
bhaskerrai <brai285@gmail.com>
2023-04-09 14:44:54 +0500

    first commit

---
+++ components/Hero.jsx
@@ -0,0 +1,17 @@
+'use client';
+
+// import { motion } from 'framer-motion';
+
+// import styles from '../styles';
+// import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+
+export default Hero;
+
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,43 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
+import { Footer, Navbar } from '../components';
+import { About, Explore, Hero, Feedback, GetStarted , Insights, WhatsNew, World } from '../sections';
 
 
 export default function Home() {
   return (
-    <>
-      Hello
-    </>
+    <div className='bg-primary-black overflow-hidden'>
+      
+      <Head>
+        <title>Metaversus</title>
+      </Head>
+
+      <Navbar />
+      
+      <Hero />
+      
+      <div className='relative'>
+        <About />
+        <div className='gradient-03 z-0'/>
+        <Explore />
+      </div>
+
+      <div className='relative'>
+        <GetStarted />
+        <div className='gradient-04 z-0'/>
+        <WhatsNew />
+      </div>
+      
+      <World />
+
+      <div className='relative'>
+        <Insights />
+        <div className='gradient-04 z-0'/>
+        <Feedback />
+      </div>
+      
+      <Footer />
+    </div>
   )
 }
